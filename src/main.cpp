2c8048c8b402289a42ca2ec77d3c06cdc6062e50
Miitto <67914241+Miitto@users.noreply.github.com>
2025-10-31 23:01:49 +0000

    style: Log on start and exit

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
-#include "gl/gl.hpp"
 #include "logger/logger.hpp"
 #include "renderer.hpp"
-#include <engine/gui.hpp>
-#include <engine/input.hpp>
-#include <engine/window.hpp>
-#include <imgui/imgui.h>
 
 int main() {
+  Logger::info("Starting application...");
   Renderer app(800, 600, "CSC8502 FLOAT");
 
-  return engine::run(app);
+  int r = engine::run(app);
+
+  if (r != 0) {
+    Logger::error("Application exited with error code {}", r);
+  } else {
+    Logger::info("Application exited successfully");
+  }
+
+  return r;
 }
